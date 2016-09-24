7f98c3b2b2a6e43b57380c8e22a6f037dc9c030c
lglfa <lglfa666@gmail.com>
2016-09-24 23:34:08 +0800

    new dic

--- weixinInterface.py
+++ weixinInterface.py
@@ -40,46 +40,6 @@ class WeixinInterface:
         # 如果是来自微信的请求，则回复echostr
         if hashcode == signature:
             return echostr
-    #打开柯林斯
-    def openCol(self):
-        # 打开柯林斯英汉词典
-        with open('En-Ch CollinsCOBUILD.txt', 'r') as f:
-            readdata = f.read()
-        return self.allResult(readdata)
-    #打开剑桥词典
-    def openCam(self):
-        # 剑桥
-        with open('En-Ch_Cambridge_Advanced_Learner_Dictionary.txt', 'r') as f3:
-            readdata3 = f3.read()
-        return self.allResult(readdata3)
-        #打开所有字典
-    def openLastDic(self):
-        # 牛津英汉词典
-        with open('En-Ch_Oxford_Advanced_Leaner_Dictionary.txt', 'r') as f1:
-            readdata1 = f1.read()
-        # 朗曼
-        with open('En-Ch_Longman_Dictionary_of_Contemporary_English.txt', 'r') as f2:
-            readdata2 = f2.read()
-        return self.allResult(readdata1)+self.allResult(readdata2)
-
-    def allResult(self,readdata):
-        # 微信发来的content为unicode
-        content2 = ' '.join(content.split())
-        # 判断中英文
-        if content[0] >= u'\u4e00' and content[0] <= u'\u9fa5':
-            content_8 = content.encode('utf-8')
-            reExpre = "\n.{0,100}" + content_8 + ".{0,200}\n"
-            allApe = re.findall(reExpre, readdata)
-
-        # 有大写优先大写，包含小写
-        elif content[0] >= 'A' and content[0] <= 'Z':
-            reExpre = "\n.{0,100} " + content2 + " .{0,200}\n"
-            reExpre1 = "\n.{0,100} " + content2.lower() + ".{0,200}\n"
-            allApe = re.findall(reExpre, readdata) + re.findall(reExpre1, readdata)
-        else:
-            reExpre = "\n.{0,100} " + content2 + " .{0,200}\n"
-            allApe = re.findall(reExpre, readdata)
-        return allApe
 
     def POST(self):
         str_xml = web.data()  # 获得post来的数据
@@ -89,34 +49,52 @@ class WeixinInterface:
         fromUser = xml.find("FromUserName").text
         toUser = xml.find("ToUserName").text
 
-       # 回复查找的内容
-        allApe1 = self.openCam()
+        # 柯林斯英汉词典
+        with open('En-Ch CollinsCOBUILD.txt', 'r') as f:
+            readdata = f.read()
+        #牛津英汉词典
+        # with open('En-Ch_Oxford_Advanced_Leaner_Dictionary.txt', 'r') as f1:
+        #     readdata1 = f1.read()
+        #朗曼
+        # with open('En-Ch_Longman_Dictionary_of_Contemporary_English.txt', 'r') as f2:
+        #     readdata2 = f2.read()
+        #剑桥
+        with open('En-Ch_Cambridge_Advanced_Learner_Dictionary.txt', 'r') as f3:
+            readdata3 = f3.read()
 
-        if len(allApe1)>=6:
-            allApes =allApe1
+        # 微信发来的content为unicode
+        content2 = ' '.join(content.split())
+        # 判断中英文
+        if content[0] >= u'\u4e00' and content[0] <= u'\u9fa5':
+            content_8 = content.encode('utf-8')
+            reExpre = "\n.{0,100}" + content_8 + ".{0,200}\n"
+            allApes = re.findall(reExpre, readdata)+re.findall(reExpre, readdata3)
+
+        # 有大写优先大写，包含小写
+        elif content[0] >= 'A' and content[0] <= 'Z':
+            reExpre = "\n.{0,100} " + content2 + " .{0,200}\n"
+            reExpre1 = "\n.{0,100} " + content2.lower() + ".{0,200}\n"
+            allApes = re.findall(reExpre, readdata) + re.findall(reExpre, readdata3) \
+                      +re.findall(reExpre1, readdata) + re.findall(reExpre1, readdata3)
         else:
-            allApe2 = self.openCam()
-            allApes = allApe1+allApe2
-                if len(allApes)>=6:
-                    pass
+            reExpre = "\n.{0,100} " + content2 + " .{0,200}\n"
+            allApes = re.findall(reExpre, readdata)+re.findall(reExpre, readdata3)
+
+        # 回复查找的内容
+        if allApes:
+            strip_str = u'■'.encode('utf-8')
+            j = 1
+            reply_content = ""
+            for i in allApes:
+                if i[1:4] == strip_str:
+                    reply_content = reply_content + strip_str + "  " + i.strip('\n').strip(strip_str) + '\n\n'
+                    j += 1
                 else:
-                    allApe3 = self.openLastDic()
-                    allApes = allApes +allApe3
-            if allApes:
-                strip_str = u'■'.encode('utf-8')
-                j = 1
-                reply_content = ""
-                for i in allApes:
-                    if i[1:4] == strip_str:
-                        reply_content = reply_content + strip_str + "  " + i.strip('\n').strip(strip_str) + '\n\n'
-                        j += 1
-                    else:
-                        reply_content = reply_content + strip_str + "  " + i.strip('\n') + '\n\n'
-                        j += 1
-                    if j > 6:
-                        break
-            else:
-                reply_content = 'Sorry, your search didn\'t match any dictionaries'
+                    reply_content = reply_content + strip_str + "  " + i.strip('\n') + '\n\n'
+                    j += 1
+                if j > 6:
+                    break
+        else:
+            reply_content = 'Sorry, your search didn\'t match any dictionaries'
 
-            return self.render.reply_text(fromUser, toUser, int(time.time()), reply_content)
-
+        return self.render.reply_text(fromUser, toUser, int(time.time()), reply_content)
