7d62afa208ff82c5b31906162516a440aea5e756
lglfa <lglfa666@gmail.com>
2016-09-29 00:47:06 +0800

    add auto reply when subscribe

--- weixinInterface.py
+++ weixinInterface.py
@@ -44,13 +44,23 @@ class WeixinInterface:
     def POST(self):
         data = web.data()  # 获得post来的数据
         xml = etree.fromstring(data)  # 进行XML解析
-        content = xml.find("Content").text  # 获得用户所输入的内容
+        xml_content = xml.find("Content")
         msgType = xml.find("MsgType").text
         fromUser = xml.find("FromUserName").text
         toUser = xml.find("ToUserName").text
-
-
-        # ------------------------------先用这两个词典，保证回复速度--------------------------------#
+        if xml_content:
+            content = xml.find("Content").text
+        elif msgType =="event":
+            msg = xml.find('Event').text
+            if msg =="subscribe":
+                return self.render.reply_text(fromUser,toUser,int(time.time()), u"■ 精选柯林斯高阶、牛津高阶、剑桥高阶和朗曼当代四部权威字典例句\n"
+                                                                                u"■ 发送中英文词组即可获得相关的例句\n"
+                                                                                u"■ 随机返回6句如果没有想要的，再发一遍试试\n"
+                                                                                u"■ 支持正则输入，譬如：\n"
+                                                                                u"""  1)  admit.*  匹配admit, admitted等。"."匹配任意字符，"*"重复前面\n\
+                                                                                  2)  admit.*承认  匹配admit与承认有关的例句\n\
+                                                                                  3)  get \w*ed 匹配get married, get killed等; "\w"表任意英文字符""")
+            # ------------------------------先用这两个词典，保证回复速度--------------------------------#
         # 柯林斯英汉词典
         with open('En-Ch CollinsCOBUILD.txt', 'r') as f:
             readdata = f.read()
